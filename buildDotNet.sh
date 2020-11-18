d1fa56c54e28cb4c96c3325c1ee29f0fb64ac963
Medhi <medhi.foulgoc@gmail.com>
2020-11-18 17:04:32 +0100

    update CSPROJ Builder

--- buildDotNet.sh
+++ buildDotNet.sh
@@ -2,6 +2,7 @@
 
 clear
 ls -l /app/server/TowersWebsocketNet31
+cp ./server/TowersWebsocketNet31/TowersWebsocketNet31.csproj ./server/TowersWebsocketNet31/TowersWebsocketNet31Server.csproj
 dotnet clean
 dotnet build ./server/TowersWebsocketNet31/TowersWebsocketNet31Server.csproj
 chmod +x /app/server/TowersWebsocketNet31/towers.env
